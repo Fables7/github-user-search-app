30ff6c9062f8b33ef22df01b186783f1c1e1ddc2
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2023-09-24 19:39:18 +0100

    Added text stylings to body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { SearchBar, ToggleTheme } from "../components";
 export default function Home() {
   return (
     <main className=" text-black dark:text-blue-700">
+      hello world
       <h1>The Octocat</h1>
       <h2>The Octocat</h2>
       <ToggleTheme />
